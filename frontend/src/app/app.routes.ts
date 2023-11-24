78b214f6db3b5c38067be8c086f5e7b255c2bd03
Деникаев Эмиль Рамилевич <e.denikaev@gulliver-ul.local>
2023-11-24 20:11:06 +0400

    pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+      },
+      { 
+        path: 'upload', 
+        loadChildren: () => import('./pages/upload/upload.module').then(m => m.UploadModule),
+      },
+      { 
+        path: 'obrabotka', 
+        loadChildren: () => import('./pages/obrabotka/obrabotka.module').then(m => m.ObrabotkaModule),
+      }
+];
---
+++ frontend/src/app/pages/obrabotka/obrabotka.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-obrabotka',
+  templateUrl: './obrabotka.component.html',
+  styleUrls: ['./obrabotka.component.less']
+})
+export class ObrabotkaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
